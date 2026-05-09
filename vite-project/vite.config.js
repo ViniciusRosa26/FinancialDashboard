f648b76f40ab9111788c3984e1eb8c852bcf3e0e
ViniciusRosa26 <viniciuscor4321@gmail.com>
2026-05-09 11:10:13 -0300

    docker compose aws setup

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     strictPort: true,
     proxy: {
-      '/overviews': 'http://localhost:8080',
-      '/expenses': 'http://localhost:8080',
-      '/users': 'http://localhost:8080',
-      '/heritages': 'http://localhost:8080',
-      '/incomes': 'http://localhost:8080',
+      '/overviews': 'http://backend:8080',
+      '/expenses': 'http://backend:8080',
+      '/users': 'http://backend:8080',
+      '/heritages': 'http://backend:8080',
+      '/incomes': 'http://backend:8080',
     },
   },
 })
